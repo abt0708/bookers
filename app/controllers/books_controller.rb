33816b35df4ff95abec4ae6beaa458d0a69f1be5
abt0708 <203622393+abt0708@users.noreply.github.com>
2025-04-13 07:43:27 +0000

    [finish]edit,update,destroy機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
     # 3. データをデータベースに保存するためのsaveメソッド実行
     book.save
     # 4. トップ画面へリダイレクト
-    redirect_to "/books/#{book.id}"
+    redirect_to book_path(book.id)
   end
 
   def index
@@ -21,6 +21,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト 
   end
 
   private
