20cb6e4e95523550b9cdc29562f71c739c362d9d
abt0708 <203622393+abt0708@users.noreply.github.com>
2025-04-13 13:27:29 +0000

    [finish]バリデーションの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,15 +5,16 @@ class BooksController < ApplicationController
 
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    if book.save
+    if @book.save
       # 3. フラッシュメッセージを定義
       flash[:notice] = "Book was successfully created"
       # 4. トップ画面へリダイレクト
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      render :new
+      @books = Book.all
+      render :index
     end
   end
 
@@ -31,13 +32,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
        # 3. フラッシュメッセージを定義
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      render :new
+      render :edit
     end
   end
 
