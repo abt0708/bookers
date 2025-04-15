230d7d59f29c8c2d04be8db086185b3af31a621c
abt0708 <203622393+abt0708@users.noreply.github.com>
2025-04-15 08:59:47 +0000

    [finish]テストファイル準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
   root 'homes#top'
-  get 'books/new' => 'books#new'
   resources :books
-   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # get '/top' => 'homes#top'
-  # post 'books' => 'books#create'
-  # get 'books' => 'books#index'
-  # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  # get 'books/:id' => 'books#show',as:'book'
-  # patch 'books/:id' => 'books#update', as: 'update_book'
-  # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
